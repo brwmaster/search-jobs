71440e4125591108f0e302794174ab7d8d455db4
Edwin Boon <edwin.boon@eo.nl>
2023-11-17 11:28:19 +0100

    Few small changes

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
 <template>
-  <div>
-    <main-nav />
-    <h1 class="mt-8 text-6xl text-center">Search Jobs</h1>
-  </div>
+  <MainNav />
 </template>
 
 <script>
-  import MainNav from '@/components/MainNav.vue'
-  
-  export default {
-    name: "App",
-    components: {
-      MainNav
-    }
-  }
+import MainNav from "@/components/MainNav.vue";
+
+export default {
+  name: "App",
+  components: {
+    MainNav,
+  },
+};
 </script>
