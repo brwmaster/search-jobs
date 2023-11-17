3410e466c09688d20cf4a39ce80a0176c3249570
Edwin Boon <edwin.boon@eo.nl>
2023-11-17 11:12:00 +0100

    Add MainNav component

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,17 @@
 <template>
-  <h1 class="mt-8 text-6xl text-center">Search Jobs</h1>
+  <div>
+    <main-nav />
+    <h1 class="mt-8 text-6xl text-center">Search Jobs</h1>
+  </div>
 </template>
+
+<script>
+  import MainNav from '@/components/MainNav.vue'
+  
+  export default {
+    name: "App",
+    components: {
+      MainNav
+    }
+  }
+</script>
